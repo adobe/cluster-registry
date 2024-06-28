3cba0494ac1732de9ee7a4016e048995fdf7c199
aalexand <aalexand@adobe.com>
2024-06-28 11:37:41 +0300

    Add descriptive comments to event types

--- pkg/sqs/event.go
+++ pkg/sqs/event.go
@@ -7,7 +7,17 @@ import (
 )
 
 const (
-	ClusterUpdateEvent        = "cluster-update"
+	MessageAttributeType = "Type"
+
+	// ClusterUpdateEvent refers to an update of the Cluster object that
+	// is sent by the client controller. This event is sent to the SQS queue and
+	// is consumed by the API server which reconciles the DB.
+	ClusterUpdateEvent = "cluster-update"
+
+	// PartialClusterUpdateEvent refers to an update of the ClusterSync object on the
+	// management cluster which is sent by the sync controller. This event is sent to
+	// the SQS queue and is consumed by the sync client which creates/updates the
+	// Cluster object on the cluster.
 	PartialClusterUpdateEvent = "partial-cluster-update"
 )
 
@@ -21,10 +31,10 @@ func NewEvent(msg *awssqs.Message) (*Event, error) {
 		return nil, errors.New("empty message")
 	}
 	// check if type is set
-	if msg.MessageAttributes["Type"] == nil {
+	if msg.MessageAttributes[MessageAttributeType] == nil {
 		return nil, errors.New("missing event type")
 	}
-	eventType := *msg.MessageAttributes["Type"].StringValue
+	eventType := *msg.MessageAttributes[MessageAttributeType].StringValue
 	if !slices.Contains([]string{ClusterUpdateEvent, PartialClusterUpdateEvent}, eventType) {
 		return nil, errors.New("invalid event type")
 	}
